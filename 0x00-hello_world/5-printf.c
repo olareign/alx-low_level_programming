3b8ab42c402bb2cd15b9b2d03f11b63f2fe28007
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-09 08:03:28 -0800

    my second C programming code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -print a C programe using a printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
