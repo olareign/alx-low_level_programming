e01ee8364bd51748f0734c9013aa09e5b77e6862
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-12 12:27:17 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,6 +13,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	printf("
+	printf("Last digit of %d is ", n);
+	if (x > 5)
+	{
+		printf("%d and is greater than 5\n", x);
+	}
+	else if ((x < 6) && (x < 0))
+	{
+		printf("%d and is less than 6 and not 0\n", x);
+	}
+	else
+	{
+		printf("%d and is 0\n", x);
+	}
 	return (0);
 }
