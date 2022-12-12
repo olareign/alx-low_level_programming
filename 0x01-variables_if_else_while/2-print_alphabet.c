2096582f1758b2a7a8770697e8fc97dfcf70905b
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-12 12:42:21 -0800

    stdlib.h input

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print alhabet in lowercase
+ * Return: 0
+*/
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+	return (0);
+}
