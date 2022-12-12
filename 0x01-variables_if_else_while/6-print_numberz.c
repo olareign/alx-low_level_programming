d4bd23e77479ee378b9b2cc686d3709c2fd8ac29
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-12 15:05:14 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -print all the single digit of base 10 starting from 0
+ * Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
