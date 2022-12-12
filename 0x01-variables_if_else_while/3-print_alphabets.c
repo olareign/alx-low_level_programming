3f3ab1f26941c2700c8cb3741291ac1181b9b6c6
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-12 13:12:42 -0800

    3rd task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -print an alphabets in lowercase then in uppercase
+ * Return: 0
+*/
+int main(voi)
+{
+	int char = "a";
+	int char = "A";
+
+	while (a <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (a <= 'Z')
+	{
+		puchar(CH);
+		CH++;
+	}
+}
+
