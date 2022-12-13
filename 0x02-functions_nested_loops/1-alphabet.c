416cb2052e55c0d9ccfd7ca706eb7b23ad7523b6
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-13 15:03:59 -0800

    betty config

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * main -print alphabet in lowercase 
+ * print_alphabet -print alphabet in lowercase
  * Return: 0
 */
 void print_alphabet(void)
 {
 	char a;
 
-	for ( a = 0; a <= 'z'; a++ )
+	for (a = 0; a <= 'z'; a++)
 		_putchar(a);
-	
 	_putchar('\n');
 }
