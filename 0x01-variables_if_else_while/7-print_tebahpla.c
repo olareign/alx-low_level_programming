bcf8af294211e1c100d22f10ee7e769881981f23
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-12 16:09:52 -0800

    betty headache

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-	char l;
+	char letter;
 
-	for (l = 'z'; l >= 'a'; l--)
-	       putchar(l);
-	
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
