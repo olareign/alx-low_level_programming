bf45a6f2efe69152d2d88e577d14b116f2ff9f5e
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-13 14:08:28 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -print alphabet in lowercase 
+ * Return: 0
+*/
+int main(void)
+{
+	char a = 'a';
+
+	while ( a <= 'z';)
+	{
+		_putchar('a');
+		a++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
