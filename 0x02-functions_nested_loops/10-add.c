9bd90acb808ecb35be5c0f0767af67149ef3f18a
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-14 15:05:55 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - print the result of addittion of two number
+ * @num1: first adding value
+ * @num2: second adding value
+ * Return: 0
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
