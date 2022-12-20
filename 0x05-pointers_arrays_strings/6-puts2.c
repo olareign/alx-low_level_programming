98392ed1c1d82396b10085dc0aef9c46043bc439
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-20 05:54:46 -0800

    3 error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -19,5 +19,5 @@ void puts2(char *str)
 			_putchar(str[i]);
 		i++;
 	}
-	_putchar('\0');
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,5 +23,5 @@ void puts_half(char *str)
 		i = (count - 1) / 2;
 	for (i++; i < count; i++)
 		_putchar(str[i]);
-	_putchar('\0');
+	_putchar('\n');
 }
