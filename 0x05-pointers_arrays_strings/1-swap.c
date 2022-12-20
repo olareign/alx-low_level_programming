59add79aae344b774e6609021eb444f0deaf6e2a
Abdulrasaq Taofeeq Olarewaju <abdulrasaqolarewaju88@gmail.com>
2022-12-20 04:58:07 -0800

    betty discription

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * swap_in - a function thats swap the value of two integers.
+ * swap_int - a function thats swap the value of two integers.
  * @a: first value to be provided
  * @b: Second parameter to be provided
  * int c: addressed the swapped value will be sent to
